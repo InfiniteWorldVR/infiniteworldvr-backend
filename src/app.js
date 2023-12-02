71fef31a37b70b55151cc6a57b4ef502971f7d49
Robert Niyitanga <niroberto>
2023-12-02 22:01:26 +0200

    feature/ implement

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({}));
 app.use(cors());
+app.use(upload.single("image"));
 
 // Define your routes here
 app.get("/", (req, res) => {
--- src/routes/teamRoute.js
+++ src/routes/teamRoute.js
@@ -2,12 +2,12 @@ import { Router } from "express";
 import express from "express";
 
 import teamController from "../controllers/teamController";
-import upload from "../helper/multer";
+// import upload from "../helper/multer";
 
 const router = Router();
 
-const app = express();
-app.use(upload.single("image"));
+// const app = express();
+// app.use(upload.single("image"));
 
 router.get("/", teamController.getTeams);
 router.get("/:id", teamController.getTeamById);
