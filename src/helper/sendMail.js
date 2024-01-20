43a3c63e4f11f77795987367a37862b022590761
Robert Niyitanga <niroberto>
2024-01-20 19:12:05 +0200

    fix: send email password

--- src/helper/sendMail.js
+++ src/helper/sendMail.js
@@ -1,26 +1,23 @@
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
 
-// Create a nodemailer transporter using your SMTP server details
+
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "contact.infiniteworldvr@gmail.com",
-    pass: "uivt ysoi zfhr eick",
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
   },
 });
-
-// Reusable function to send an email
 const sendEmail = async (to, subject, text) => {
   try {
-    // Define the email content
     const mailOptions = {
       from: "contact@infiniteworldvr.com",
       to,
       subject,
       html: text,
     };
-
-    // Send the email
     const info = await transporter.sendMail(mailOptions);
     console.log("Email sent:", info.response);
   } catch (error) {
